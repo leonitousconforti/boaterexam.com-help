2541b8905cd53dab54ec67ca335905e335f96f7c
Leo Conforti <leoconforti@ltgk.net>
2026-07-03 11:08:47 -0500

    save

---
+++ tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+    entry: ["src/course-content.ts", "src/exam.ts"],
+    format: ["esm"],
+    dts: false,
+    sourcemap: true,
+    clean: true,
+    splitting: false,
+});
