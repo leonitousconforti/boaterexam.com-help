b5a1d39aba8fe5990de8960c32ad4bfbbf16d69c
Leo Conforti <leoconforti@ltgk.net>
2026-07-03 15:22:45 -0500

    maybe have exams working

--- src/exam.ts
+++ src/exam.ts
@@ -1,3 +1,5 @@
+import type * as Types from "effect/Types";
+
 import * as Effect from "effect/Effect";
 import * as Layer from "effect/Layer";
 import * as References from "effect/References";
@@ -7,13 +9,16 @@ import * as KeyValueStore from "effect/unstable/persistence/KeyValueStore";
 import * as BrowserKeyValueStore from "@effect/platform-browser/BrowserKeyValueStore";
 import * as BrowserRuntime from "@effect/platform-browser/BrowserRuntime";
 
-const current_question = 1;
+// const LocationSchema = Schema.TemplateLiteralParser([
+//     "https://www.boaterexam.com/quizzes/",
+//     Schema.FiniteFromString,
+//     "/",
+// ]);
 
-const LocationSchema = Schema.TemplateLiteralParser(["https://www.boaterexam.com/quizzes/", Schema.NumberFromString]);
-const AnswerSchema = Schema.Struct({ elementId: Schema.String, content: Schema.String });
+const ChapterSchema = Schema.TemplateLiteralParser(["Chapter ", Schema.FiniteFromString, " Quiz", Schema.String]);
 const ExamSchema = Schema.Record(
     Schema.String,
-    Schema.Array(AnswerSchema).check(Schema.isMinLength(1), Schema.isMaxLength(4))
+    Schema.Array(Schema.String).check(Schema.isMinLength(1), Schema.isMaxLength(4))
 ).pipe(Schema.fromJsonString);
 
 const AppLive = Layer.mergeAll(
@@ -21,36 +26,120 @@ const AppLive = Layer.mergeAll(
     Layer.succeed(References.MinimumLogLevel, "All")
 );
 
-Effect.gen(function* () {
+const loadExam: Effect.Effect<
+    Schema.Schema.Type<typeof ExamSchema>,
+    KeyValueStore.KeyValueStoreError | Schema.SchemaError,
+    KeyValueStore.KeyValueStore
+> = Effect.gen(function* () {
     const storage = yield* KeyValueStore.KeyValueStore;
 
-    const exam = Schema.decodeUnknownSync(LocationSchema)(window.location.href)[1];
-    const maybeCurrentExam = yield* storage.get(`boaterexam.com-help_exam_${exam}`);
-    const currentExam = maybeCurrentExam === undefined ? {} : yield* Schema.decodeEffect(ExamSchema)(maybeCurrentExam);
+    const currentChapterString = document.querySelector<HTMLTitleElement>("title")!.innerText;
+    const [_, currentChapter, __] = yield* Schema.decodeUnknownEffect(ChapterSchema)(currentChapterString);
 
-    const questionText = document
-        .querySelector<HTMLLegendElement>(`#question_${current_question} #question-text`)!
-        .textContent.trim();
+    const examString = yield* storage.get(`boaterexam.com-help_exam_${currentChapter}`);
+    return examString === undefined ? {} : yield* Schema.decodeEffect(ExamSchema)(examString);
+});
 
-    const answerLabels = document.querySelectorAll<HTMLLabelElement>(
-        `#question_${current_question} .assessment-item label`
+const writeExam = (
+    exam: Types.Mutable<Schema.Schema.Type<typeof ExamSchema>>
+): Effect.Effect<void, KeyValueStore.KeyValueStoreError | Schema.SchemaError, KeyValueStore.KeyValueStore> =>
+    Effect.gen(function* () {
+        const storage = yield* KeyValueStore.KeyValueStore;
+
+        const currentChapterString = document.querySelector<HTMLTitleElement>("title")!.innerText;
+        const [_, currentChapter, __] = yield* Schema.decodeUnknownEffect(ChapterSchema)(currentChapterString);
+
+        const updatedExamString = yield* Schema.encodeEffect(ExamSchema)(exam);
+        yield* storage.set(`boaterexam.com-help_exam_${currentChapter}`, updatedExamString);
+    });
+
+const takeExam = Effect.gen(function* () {
+    const instructions = document.querySelector<HTMLDivElement>("#instructions")!;
+    if (instructions.checkVisibility()) {
+        const startAssessmentButton = document.querySelector<HTMLButtonElement>("#instructions button")!;
+        startAssessmentButton.click();
+    }
+
+    const currentQuestionString = document.querySelector<HTMLDivElement>(".current_question")!.textContent;
+    const currentQuestion = yield* Schema.decodeEffect(Schema.FiniteFromString)(currentQuestionString);
+
+    const totalQuestionsString = document.querySelector<HTMLDivElement>(".total_questions")!.textContent;
+    const totalQuestions = yield* Schema.decodeEffect(Schema.FiniteFromString)(totalQuestionsString);
+
+    for (let questionNumber = currentQuestion; questionNumber <= totalQuestions; questionNumber++) {
+        const exam = yield* loadExam;
+
+        const questionText = document
+            .querySelector<HTMLLegendElement>(`#question_${questionNumber} #question-text`)!
+            .textContent.trim();
+
+        const answerLabels = Array.from(
+            document.querySelectorAll<HTMLLabelElement>(`#question_${questionNumber} .assessment-item label`)
+        );
+
+        const answersText = answerLabels.map((label) => label.textContent.trim()).sort();
+        const questionKey = `${questionText}-${answersText.join("-")}`;
+
+        const updatedExam = exam[questionKey] !== undefined ? exam : { ...exam, [questionKey]: answersText };
+        yield* writeExam(updatedExam);
+
+        const randomAnswer = updatedExam[questionKey][Math.floor(Math.random() * updatedExam[questionKey].length)];
+        const randomAnserInput = answerLabels.find((label) => label.textContent.trim() === randomAnswer)!;
+
+        yield* Effect.sleep(1000);
+        randomAnserInput.click();
+
+        const submitButton = document.querySelector<HTMLInputElement>(`#question_${questionNumber} [name="commit"]`)!;
+        yield* Effect.sleep(1000);
+        submitButton.click();
+    }
+
+    const viewResultsButton = Array.from(document.querySelectorAll<HTMLButtonElement>("button")).find(
+        (button) => button.textContent.trim() === "View Results"
+    )!;
+
+    while (!viewResultsButton.checkVisibility()) {
+        yield* Effect.sleep(300);
+    }
+
+    viewResultsButton.click();
+});
+
+const reviewExam = Effect.gen(function* () {
+    const exam = yield* loadExam;
+    const critiques = document.querySelectorAll<HTMLDivElement>(".critique-item");
+    for (const question of critiques) {
+        const questionText = question
+            .querySelector<HTMLDivElement>(".critique-question div:nth-child(2)")!
+            .textContent.trim();
+
+        const correctAnswer = Array.from(question.querySelectorAll<HTMLDivElement>(".critique-anwser"))
+            .find((div) => div.querySelector<HTMLImageElement>('img[alt="Correct"]') !== null)!
+            .textContent.trim();
+
+        const incorrectAnswers = Array.from(question.querySelectorAll<HTMLDivElement>(".critique-anwser"))
+            .filter((div) => div.querySelector<HTMLImageElement>('img[alt="Incorrect"]') !== null)
+            .map((div) => div.innerText.trim())
+            .map((text) => text.replace("\nYour Answer", ""));
+
+        const allAnswersSorted = [correctAnswer, ...incorrectAnswers].sort();
+        const questionKey = `${questionText}-${allAnswersSorted.join("-")}`;
+        (exam as Types.Mutable<typeof exam>)[questionKey] = [correctAnswer];
+    }
+
+    yield* writeExam(exam);
+    const nextButton = document.querySelector<HTMLAnchorElement>(".course-controls-group a:nth-of-type(2)")!;
+    nextButton.click();
+});
+
+Effect.gen(function* () {
+    const viewCritiqueButton = Array.from(document.querySelectorAll<HTMLButtonElement>("button")).find(
+        (button) => button.textContent.trim() === "View Critique"
     );
 
-    const answers = Array.from(answerLabels).map((label) => ({
-        elementId: label.id,
-        content: label.textContent.trim(),
-    }));
-
-    const updatedExam =
-        currentExam[questionText] !== undefined ? currentExam : { ...currentExam, [questionText]: answers };
-    const randomAnswer = updatedExam[questionText][Math.floor(Math.random() * updatedExam[questionText].length)];
-
-    const updatedExamString = yield* Schema.encodeEffect(ExamSchema)(updatedExam);
-    yield* storage.set(`boaterexam.com-help_exam_${exam}`, updatedExamString);
-
-    const randomAnserInput = document.querySelector<HTMLLabelElement>(`#${randomAnswer.elementId}`)!;
-    setTimeout(() => randomAnserInput.click(), 1000);
-
-    const submitButton = document.querySelector<HTMLInputElement>(`#question_${current_question} [name="commit"]`)!;
-    setTimeout(() => submitButton.click(), 2000);
+    if (viewCritiqueButton && viewCritiqueButton.checkVisibility()) {
+        yield* reviewExam;
+    } else {
+        yield* takeExam;
+    }
 }).pipe(Effect.provide(AppLive), BrowserRuntime.runMain);
