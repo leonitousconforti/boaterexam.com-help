ce8307801a2ace67bf76fea139947d1d342b58a1
Leo Conforti <leoconforti@ltgk.net>
2026-07-03 12:06:01 -0500

    add noexternal

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "tsup";
 export default defineConfig({
     entry: ["src/course-content.ts", "src/exam.ts"],
     format: ["esm"],
+    bundle: true,
+    splitting: false,
+    noExternal: [/./],
     dts: false,
     sourcemap: true,
     clean: true,
